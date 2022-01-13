3facdb128026d3739f44ee26dabe919281053376
Aman <amankhanna224466@gmail.com>
2022-01-13 15:38:26 +0530

    joi validation

--- controllers/auth/signup-controller.js
+++ controllers/auth/signup-controller.js
@@ -1,5 +1,6 @@
 const HashingService = require("../../services/hashing-service");
 const UserService = require("../../services/user-service");
+const Joi = require('joi');
 
 const signupController = () => {
     return {
@@ -8,6 +9,23 @@ const signupController = () => {
 
             if (!name || !email || !tel || !password) {
                 return res.status(422).json({ err: 'Please fill all the fields!' })
+            }
+
+            const joiSchema = Joi.object({
+                name: Joi.string().min(2).required()/* .regex(new RegExp('^[a-zA-Z](3-30)$')) */,
+                email: Joi.string().min(2).email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'in'] } }).required(),
+                tel: Joi.string().min(2).required()/* .regex('^[0-9](8-12)$') */,
+                password: Joi.string().min(2).required()
+            })
+
+            const validateRequest = joiSchema.validate(req.body)
+            
+            if(validateRequest.error) {
+                const errorType = validateRequest.error.name;
+                const errorMessage = validateRequest.error.message;
+                if (errorType == 'ValidationError') {
+                    return res.status(422).json({ err: errorMessage })
+                }
             }
 
             const user = await UserService.findUser({ $or: [{ email }, { tel }] })
